f57cc8105247ed93b954c8038c67a03af8b1abaa
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2022-05-15 21:47:39 +0200

    added blog models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    author = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+    )
+    # user_id =
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.comment
