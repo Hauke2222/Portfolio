15a4c9cd8ba9db9e765fe8ef1cf14d63dfc921ce
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2022-06-16 21:31:29 +0200

    add tech stack model field + styling update

--- projects/models.py
+++ projects/models.py
@@ -12,6 +12,7 @@ class Project(models.Model):
     source_code = models.CharField(max_length=250, blank=True)
     project_url = models.CharField(max_length=250, blank=True)
     project_start_date = models.DateField(blank=True, null=True)
+    tech_stack = models.CharField(max_length=250, blank=True)
 
     def __str__(self):
         return self.title
