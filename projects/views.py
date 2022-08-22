73b1ffa7445bd8d2cc0b05921cf9d208a82cfcad
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2022-08-22 15:27:52 +0200

    add mvp css

--- projects/views.py
+++ projects/views.py
@@ -7,7 +7,7 @@ from .models import Project
 
 def index(request):
     projects = get_list_or_404(Project.objects.all().order_by("-project_start_date"))
-    paginator = Paginator(projects, 2)
+    paginator = Paginator(projects, 10)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     return render(request, "projects/index.html", {"page_obj": page_obj})
