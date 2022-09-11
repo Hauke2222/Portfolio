bc6567a4718f9bf05acf333e86903bf6845abe9a
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2022-09-11 12:16:09 +0200

    remove contact app + templates

--- contact/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class ContactForm(forms.Form):
-    name = forms.CharField(required=True, max_length=500)
-    email = forms.EmailField(required=True, max_length=500)
-    message = forms.CharField(widget=forms.Textarea, required=True, max_length=5000)
--- contact/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-# urlpatterns = [
-#     path("", views.contact, name="contact"),
-#     path("submit", views.submit, name="submit"),
-# ]
