f8b352648d939664e40919a09afc6e88ea88175e
Hauke2222 <49106346+Hauke2222@users.noreply.github.com>
2022-09-14 13:52:58 +0200

    delete contactform

--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from .models import Project
-from .forms import ContactForm
 
 # Create your views here.
 
 
 def index(request):
     projects = get_list_or_404(Project.objects.all().order_by("-project_start_date"))
-    form = ContactForm()
-    return render(request, "projects/index.html", {"projects": projects, "form": form})
+    return render(request, "projects/index.html", {"projects": projects})
 
 
 def detail(request, pk):
